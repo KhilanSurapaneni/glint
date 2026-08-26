735ae230566ade5703c4868b62015e33b852a9aa
Khilan Surapaneni <Khilan.Surapaneni@intusurg.com>
2026-08-26 15:08:46 -0700

    created ios capture app

---
+++ capture/GlintCapture/GlintCapture/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GlintCapture
+//
+//  Created by Khilan Surapaneni on 8/26/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ capture/GlintCapture/GlintCapture/GlintCaptureApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlintCaptureApp.swift
+//  GlintCapture
+//
+//  Created by Khilan Surapaneni on 8/26/26.
+//
+
+import SwiftUI
+
+@main
+struct GlintCaptureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
